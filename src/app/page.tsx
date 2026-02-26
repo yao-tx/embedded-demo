6b2b688f6baa905de34ca79ef22632693d811e2b
TengXiong Yao <tyao@fastspring.com>
2026-02-27 03:48:05 +0800

    wrapped checkout component in suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { Checkout } from "@/components/checkout";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <div className="m-auto w-full max-w-lg p-5 rounded-md bg-neutral-100">
-        <Checkout />
+        <Suspense fallback={<p>Loading...</p>}>
+          <Checkout />
+        </Suspense>
       </div>
     </main>
   );
